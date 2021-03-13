78d8f59f3419dca55f51f57af857cc8b82eabdca
CAHOCA <c-y-h-c702@hotmail.com>
2021-03-13 11:19:56 -0500

    Submenú, únicamente unisex

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,22 +2,99 @@ import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux'
 import styles from './Sidebar.module.scss'
 import { NavLink, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
 
-import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SubMenu,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-  } from 'react-pro-sidebar';
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader,  SidebarFooter,  SidebarContent} from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import { MAN, WOMAN, BOY, COLLECTIONS, US, NEWS, CONTACT, Unisex } from '../../consts/clothe-names';
 import Footer from '../footer/Footer';
+import { PbyService } from '../../services/pby-services';
+import { addProductsAction, setFilterProductsAction, addArticlesAction, addMenuAction, setShowLoginAction, setProductsAction, setSessionAction } from '../../store/actions';
 
-const Sidebar = ({toggled, handleToggleSidebar, dataCompany}) =>{
+const Sidebar = ({products, toggled, handleToggleSidebar, dataCompany}) =>{
+const [productTypes, setProductTypes] = useState<any[]>([])
+const [itemHover, setItemHover] = useState('')
+const dispatch = useDispatch();
 
+ useEffect(() => {
+    getAllProducts()
+    getAllArticles()
+    getMenuItems()
+  }, [])
+
+
+    useEffect(() => {
+    
+
+    // if (targetRef.current) {
+    //   setDimensions({
+    //     width: targetRef.current.offsetWidth,
+    //     height: targetRef.current.offsetHeight
+    //   });
+
+    //   console.log(targetRef.current.offsetWidth);
+    // }
+
+    // console.log(wd);
+
+    if (products.products.length === 0) return
+
+    // filtro para colecciones
+    if (itemHover === COLLECTIONS) {
+      let productTypesMap: any[] = [...new Map(products.products.map(item => [item.Nombre_Coleccion, item])).values()];
+      productTypesMap = productTypesMap.map(item => {
+        return { categoria: COLLECTIONS, subCategoria: item.Nombre_Coleccion, imagen: item.Image_Colecccion }
+      })
+      setProductTypes(productTypesMap)
+      return
+    }
+
+    // Resto de categorias
+    let prodFilter
+    console.log(itemHover );
+    if (itemHover === BOY) {
+      prodFilter = products.products.filter(item => item.Sexo === itemHover)
+      
+    } else {
+      prodFilter = products.products.filter(item => item.Sexo === itemHover || item.Sexo === Unisex)
+      console.log(prodFilter);
+    }
+
+
+    let productTypesMap: any[] = [...new Map(prodFilter.map(item => [item.Tipo_Producto, item])).values()];
+    productTypesMap = productTypesMap.map(item => {
+      // console.log(`'categoria': ${item.Sexo}, 'subCategoria': ${item.Tipo_Producto}, 'imagen': ${item.Imagen_Tipo_Producto}`);
+      return { categoria: item.Sexo, subCategoria: item.Tipo_Producto, imagen: item.Imagen_Tipo_Producto }
+    })
+
+    setProductTypes(productTypesMap)
+  }, [products, itemHover])
+
+    const getAllProducts = () => {
+    PbyService.getAllProducts().then(products => {
+      if (!products) return
+      dispatch(addProductsAction(products))
+    })
+  }
+
+  const getAllArticles = () => {
+    PbyService.getAllArticles().then(articles => {
+      if (!articles) return
+      dispatch(addArticlesAction(articles))
+    })
+  }
+
+  const getMenuItems = () => {
+    PbyService.getMenu().then(menu => {
+      if (!menu) return
+      dispatch(addMenuAction(menu))
+    })
+  }
+
+ const setFilterSubmenu = (param: string | null) => {
+    console.log(param);
+    dispatch(setFilterProductsAction(param))
+  }
 
     return (
       <>
@@ -46,7 +123,34 @@ const Sidebar = ({toggled, handleToggleSidebar, dataCompany}) =>{
       </SidebarHeader>
       <SidebarContent>
             <Menu iconShape="circle">
-                <MenuItem title={MAN.toUpperCase()} >
+
+                <SubMenu title={MAN.toUpperCase()}>                 
+
+                    {productTypes.map((item, i) => (
+                      <MenuItem>
+                        <NavLink key={i} to={`/${item.categoria?.toLowerCase()=='unisex'?'unisex?s='+MAN:item.categoria.toLowerCase()}`} className={styles.item_categoria} onClick={() => {
+                                // {item.subCategoria}
+                              setFilterSubmenu(item.subCategoria)
+                              // setShowPasarela(false)
+                            }}>{item.subCategoria}
+                      </NavLink>
+                   </MenuItem> ))}
+                 
+                </SubMenu>
+
+                <SubMenu title={WOMAN.toUpperCase()}>
+                  {productTypes.map((item, i) => (
+                      <MenuItem>
+                        <NavLink key={i} to={`/${item.categoria?.toLowerCase()=='unisex'?'unisex?s='+WOMAN:item.categoria.toLowerCase()}`} className={styles.item_categoria} onClick={() => {
+                                // {item.subCategoria}
+                              setFilterSubmenu(item.subCategoria)
+                              // setShowPasarela(false)
+                            }}>{item.subCategoria}
+                      </NavLink>
+                   </MenuItem> ))}
+                </SubMenu>
+
+                {/* <MenuItem title={MAN.toUpperCase()} >
                   <NavLink to="/hombre" activeClassName={styles.activeRoute}>
                       <span>{MAN.toUpperCase()}</span>
                   </NavLink>
@@ -55,7 +159,7 @@ const Sidebar = ({toggled, handleToggleSidebar, dataCompany}) =>{
                   <NavLink to="/mujer" activeClassName={styles.activeRoute}>
                       <span>{WOMAN.toUpperCase()}</span>
                   </NavLink>
-                </MenuItem>
+                </MenuItem> */}
                 <MenuItem title={COLLECTIONS.toUpperCase()} >
                   <NavLink to="/colecciones" activeClassName={styles.activeRoute}>
                       <span>{COLLECTIONS.toUpperCase()}</span>
